91a9e719b23b149cedf3b1c30320fd4ab2e52bcf
cfeliz3030 <60493376+cfeliz3030@users.noreply.github.com>
2022-02-16 23:57:43 -0800

    Add files via upload

---
+++ airflow_mini_projects-2/log_analyzer.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+def analyze_file(file,error_type):
+    """ Parses log files, returns count of specific error type and actual error. """
+
+    if error_type not in ['INFO', 'WARNING', 'ERROR']:
+        return 'Please use appropriate error type (ex.INFO, WARNING, ERROR)'
+
+    num_errors = 0
+    errors = []
+
+    with file.open() as f:
+        for line in f:
+            if error_type in line.split(' '):
+                errors.append(line)
+                num_errors += 1
+    if errors:
+        return f'Total number of errors {num_errors} \nHere are all the errors: {errors} '
+    
+    return 'No errors found.'
+
+
+
+log_dir = '/Users/christopher/airflow/logs/marketvol'
+
+
+
+file_list = [ f.resolve() for f in Path(log_dir).rglob('*.log')]
+
+for f in file_list:
+    print(analyze_file(f,'ERROR'))
