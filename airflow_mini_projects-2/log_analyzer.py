c874dd42ddeab2b1e3b63e01b960af7203543556
cfeliz3030 <60493376+cfeliz3030@users.noreply.github.com>
2022-02-17 23:13:16 -0800

    Add files via upload

--- airflow_mini_projects-2/log_analyzer.py
+++ airflow_mini_projects-2/log_analyzer.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-def analyze_file(file,error_type):
+def analyze_file(files,error_type):
     """ Parses log files, returns count of specific error type and actual error. """
 
     if error_type not in ['INFO', 'WARNING', 'ERROR']:
@@ -8,24 +8,27 @@ def analyze_file(file,error_type):
 
     num_errors = 0
     errors = []
-
-    with file.open() as f:
-        for line in f:
-            if error_type in line.split(' '):
-                errors.append(line)
-                num_errors += 1
+    for log in files:
+        with log.open() as f:
+            for line in f:
+                if error_type in line.split(' '):
+                    errors.append(line)
+                    num_errors += 1
     if errors:
-        return f'Total number of errors {num_errors} \nHere are all the errors: {errors} '
+        return f'Total number of errors: {num_errors} \nHere are all the errors: {errors} '
+    # if errors:
+    #     return num_errors, errors 
     
     return 'No errors found.'
 
 
-
 log_dir = '/Users/christopher/airflow/logs/marketvol'
-
-
 
 file_list = [ f.resolve() for f in Path(log_dir).rglob('*.log')]
 
-for f in file_list:
-    print(analyze_file(f,'ERROR'))
+# for f in file_list:
+#     print(analyze_file(f,'ERROR'))
+
+# print(f'Total number of errors {total_errors} \nHere are all the errors: {all_errors} ')
+
+print(analyze_file(file_list,'ERROR'))
